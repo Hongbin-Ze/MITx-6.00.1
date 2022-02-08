d0f8fc51d26346f4e3a28255c3c9f865b9815e1a
Tianze <83177856+Hongbin-Ze@users.noreply.github.com>
2022-02-08 22:46:23 +0800

    Create Exercise: happy.py

---
+++ Unit 1: Python Basics/Lecture_2_Core_Elements_of_Programs/Exercise: happy.py
@@ -0,0 +1,9 @@
+# Exercise: happy
+
+# (5/5 points)
+# ESTIMATED TIME TO COMPLETE: 3 minutes
+
+# Write a piece of Python code that prints out the string 'hello world' if the value of an integer variable, happy, is strictly greater than 2.
+
+if happy > 2:
+    print('hello world')
